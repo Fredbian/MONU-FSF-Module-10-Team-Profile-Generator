b02d59cd9efc45767c25a3f2bb1465a3dcf3262b
Tao Bian <biantao0519@hotmail.com>
2022-07-13 21:21:59 +1000

    create html generator and fixed some issue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const manager = require('./lib/manager')
 const generateHTML = require('./src/htmlGenerator')
 
 // team array
-let teamProfile = []
+let team = []
 
 // get data and creatprofile
 const createProfile = () => {
@@ -48,21 +48,21 @@ const createProfile = () => {
             type: 'input',
             name: 'officeNumber',
             message: "Please enter Manager's office number",
-            when: ({ role }) => role === 'Manager'
+            when: (answers) => answers.role === 'Manager'
         },
         // get github username when role is Engineer
         {
             type: 'input',
             name: 'github',
             message: "Please enter Engineer's github username",
-            when: ({ role }) => role === 'Engineer'
+            when: (answers) => answers.role === 'Engineer'
         },
         // get school name when role is Intern
         {
             type: 'input',
             name: 'school',
             message: "Please enter Intern's school name",
-            when: ({ role }) => role === 'Intern'
+            when: (answers) => answers.role === 'Intern'
         },
         // if add member member
         {
@@ -72,29 +72,36 @@ const createProfile = () => {
         }
     ]).then((answers) => {
         // create info data based on role type
+        const id = answers.id
+        const name = answers.name
+        const email = answers.email
+        const role = answers.role
+        const officeNumber = answers.officeNumber
+        const github = answers.github
+        const school = answers.school
         // role is Manager, push info to array
-        if (answers.role === 'Manager') {
-            teamProfile.push(new manager(answers.id, answers.name, answers.email, answers.officeNumber))
-        }
+        if (role === 'Manager') {
+            team.push(new manager(id, name, email, officeNumber))
+        } 
 
         // role is Engineer, push info to array
-        if (answers.role === 'Engineer') {
-            teamProfile.push(new engineer(answers.id, answers.name, answers.email, answers.github))
+        if (role === 'Engineer') {
+            team.push(new engineer(id, name, email, github))
         }
 
         // role is Intern, push info to array
-        if (answers.role === 'Intern') {
-            teamProfile.push(new intern(answers.id, answers.name, answers.email, answers.school))
+        if (role === 'Intern') {
+            team.push(new intern(id, name, email, school))
         }
 
-        console.log(teamProfile)
+        console.log(team)
         // add more member or not
         if (answers.addMore) {
             // yes, add more
             return createProfile()
         } else {
             // no, generate HTML
-            const data = generateHTML(teamProfile)
+            const data = generateHTML(team)
             fs.writeFile('./dist/index.html', data, err => {
                 if (err) {
                     console.log(`ERROR! ${err.message}`)
--- src/htmlGenerator.js
+++ src/htmlGenerator.js
@@ -1,4 +1,5 @@
-function generateHTML() {
+// function to rander HTML
+function generateHTML(team) {
     return `
 <!DOCTYPE html>
 <html lang="en">
@@ -7,32 +8,121 @@ function generateHTML() {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
     <title>Team Profile Generator</title>
 </head>
 
 <body>
-    <header class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron text-center">
-                <h1>My Team</h1>
+    <header> 
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron text-center">
+                    <h1>My Team</h1>
+                </div>
             </div>
-        </div>
+        </div>    
     </header>
     <!-- container for profile  -->
-    <main class="container">
-        <div class="row">
-
-        </div>
+    <main> 
+        <div class="container">
+            <div class="row justify-content-center">
+            ${employeeCards}
+            </div>
+        </div>    
     </main>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 </body>
-
 </html>
-
 `
 }
+
+// function for employee cards
+const employeeCards = function generateEmployeeCard(team) {
+    const cards = []
+    for (let i = 0; i < team.length; i++) {
+        const employee = team[i]
+
+        if (employee.getRole() === 'Manager') {
+            cards.push(generateManagerCard())
+        }
+        
+        if (employee.getRole() === 'Engineer') {
+            cards.push(generateEngineerCard())
+        }
+        
+        if (employee.getRole() === 'Intern') {
+            cards.push(generateInternCard())
+        }
+    }
+    const cardsContent = cards.join('')
+    return cardsString
+}
+
+// function for Manager card
+function generateManagerCard(employee) {
+    return `
+    <div class="card col-4">
+        <div class="card-header">
+            <h2 class="card-title">${employee.getName()}</h2>
+            <h3 class="card-title"><i class="bi bi-cup"></i> ${employee.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${employee.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                <li class="list-group-item">Office Number: ${employee.officeNumber}</li>            
+            </ul>
+        </div>
+    </div>
+    `
+}
+
+// function for Engineer card
+function generateEngineerCard(employee) {
+    if (employee.getRole() === 'Engineer') {
+        return `
+    <div class="card col-4">
+        <div class="card-header">
+            <h2 class="card-title">${employee.getName()}</h2>
+            <h3 class="card-title"><i class="bi bi-eyeglasses"></i> ${employee.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${employee.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                <li class="list-group-item">Github: ${employee.getGithub()}</li>            
+            </ul>
+        </div>
+    </div>    
+    `
+    }
+}
+
+// function for Intern card
+function generateInternCard(employee) {
+    if (employee.getRole() === 'Intern') {
+        return `
+    <div class="card col-4">
+        <div class="card-header">
+            <h2 class="card-title">${employee.getName()}</h2>
+            <h3 class="card-title"><i class="bi bi-mortarboard"></i> ${employee.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${employee.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
+                <li class="list-group-item">School: ${employee.getSchool()}</li>            
+            </ul>
+        </div>
+    </div>
+    `
+    }
+}
+
+
+module.exports = generateHTML
+
